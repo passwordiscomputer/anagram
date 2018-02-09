612737d5987de5a0767ca4c68b4b38bf1b14d996
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-02-09 09:06:32 -0800

    add code to pass initial test

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,6 +4,11 @@ class Anagram
   end
 
   def anagram_check(test_word)
-    test_word
+    @word = @word.split("").sort
+    test_word = test_word.split("").sort
+    if @word == test_word
+      return 'anagram'
+    end
+    return 'not an anagram'
   end
 end
